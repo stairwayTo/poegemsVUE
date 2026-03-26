15d23608ea752c4a79dc3296d4e5c056246b7169
Camille <emelcamille@gmail.com>
2026-03-26 11:16:58 +0100

    persistance du store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useWatchlistStore} from '@/stores/watchlistStore.ts'
+import { onMounted } from 'vue'
 
+const watchlistStore = useWatchlistStore()
+onMounted(() => {watchlistStore.initStore()})
 </script>
 
 <template>
--- src/stores/watchlistStore.ts
+++ src/stores/watchlistStore.ts
@@ -6,13 +6,25 @@ export const useWatchlistStore = defineStore('watchlistStore', () => {
   const watchlist = ref<GemSummary[]>([])
   function addToWatchlist(gem: GemSummary) {
     watchlist.value.push(gem)
+    persist()
   }
   function removeFromWatchlist(gemId: number) {
     watchlist.value = watchlist.value.filter(g => g.id !== gemId)
+    persist()
+  }
+  function initStore() {
+    const saved = localStorage.getItem('poegems-watchlist')
+    if (saved) watchlist.value = JSON.parse(saved)
+  }
+
+  function persist() {
+    localStorage.setItem('poegems-watchlist', JSON.stringify(watchlist.value))
   }
   return {
     watchlist,
     addToWatchlist,
     removeFromWatchlist,
+    initStore,
+    persist
   }
 })
